0743e3b345e2773a882bd5a548e3fbb25b9e8d80
Henrik Danielsson <henkedanne@gmail.com>
2017-01-26 21:00:22 +0100

    Tried different ways of getting the random number to not show twice in a row… didn’t work, but commit as save

--- main.js
+++ main.js
@@ -3,18 +3,26 @@ var quoteBtn = document.querySelector('#quote_btn');
 var twitterBtn = document.querySelector('#twit_btn');
 
 var quotes = ["Du ska inte döda", "Var som en fisk", "Du är ett ägg"];
-var rand;
-var lastRand = rand;
+var rand = 0;
+var lastRand;
+
 function randomizeQuote() {
 	
 	rand = Math.floor(Math.random() * quotes.length);
-	return quotes[rand];
-
+	lastRand =+ rand;
+	console.log("rand ", rand);
+	console.log("Last ", lastRand);
+	
+		return quotes[rand];
+	
+		
+	 
 }
 
 function getRandomQuote() {
+
 	quoteHolder.innerHTML = randomizeQuote();
-	console.log(lastRand);
+	
 };
 
 quoteBtn.addEventListener("click", getRandomQuote);
