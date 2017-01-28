a2b630894a9b1f67c38aa8c1cbe74e96640087f4
Henrik Danielsson <henkedanne@gmail.com>
2017-01-28 21:50:12 +0100

    Found a way to randomize a number and not use it twice. Don’t know if it is the most effective way. but it works.

--- main.js
+++ main.js
@@ -3,27 +3,28 @@ var quoteBtn = document.querySelector('#quote_btn');
 var twitterBtn = document.querySelector('#twit_btn');
 
 var quotes = ["Du ska inte döda", "Var som en fisk", "Du är ett ägg"];
-var rand = 0;
-var lastRand;
+var randNum = [];
+
 
 function randomizeQuote() {
-	
-	rand = Math.floor(Math.random() * quotes.length);
-	lastRand =+ rand;
-	console.log("rand ", rand);
-	console.log("Last ", lastRand);
-	
-		return quotes[rand];
-	
-		
+	if (!randNum.length) {
+		for (var i = 0; i < quotes.length; i++) {
+			randNum.push(i);
+			}
+		}
+	var index = Math.floor(Math.random() * randNum.length);
+	var rand = randNum[index];
+	console.log(randNum);
+	randNum.splice(index, 1);
+	console.log(randNum);
+	return quotes[rand];
+};		
 	 
-}
+
 
 function getRandomQuote() {
-
 	quoteHolder.innerHTML = randomizeQuote();
-	
-};
+	};
 
 quoteBtn.addEventListener("click", getRandomQuote);
 
